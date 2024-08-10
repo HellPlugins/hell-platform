27871556b29c827592a76c8f14a53d3bc4c34139
night <nightgithub@gmail.com>
2024-08-10 13:06:12 +0200

    Added SetIntents annotations

---
+++ javacord/src/main/java/org/helldev/javacord/platform/annotations/SetIntents.java
@@ -0,0 +1,12 @@
+package org.helldev.javacord.platform.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SetIntents {
+    Class<?>[] value();
+}
