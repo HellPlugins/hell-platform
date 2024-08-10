ad2a7c63fb16332ff2710cd9910c6ef607bd67fa
night <nightgithub@gmail.com>
2024-08-10 13:07:40 +0200

    Updated SetIntents annotation to use Javacord Intent

--- javacord/src/main/java/org/helldev/javacord/platform/annotations/SetIntents.java
+++ javacord/src/main/java/org/helldev/javacord/platform/annotations/SetIntents.java
@@ -1,5 +1,7 @@
 package org.helldev.javacord.platform.annotations;
 
+import org.javacord.api.entity.intent.Intent;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface SetIntents {
-    Class<?>[] value();
+    Intent[] value();
 }
